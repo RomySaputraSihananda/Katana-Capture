fa4541de6b40f8ad880fe33332e03303728b8083
romys <romysaputrasihanandaa@gmail.com>
2023-11-30 15:09:19 +0700

    add burgerbutton

---
+++ src/components/BurgerButton.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class BurgerButton extends React.Component {
+  render = (): React.ReactNode => {
+    return (
+      <div>
+        <BurgerButton />
+      </div>
+    );
+  };
+}
+
+export default BurgerButton;
